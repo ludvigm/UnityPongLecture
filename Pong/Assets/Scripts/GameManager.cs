60f677bb9bdc3769754ad75a39ba289b9fdd5270
lm222ix <lm222ix@student.lnu.se>
2016-03-08 10:51:49 +0100

    Unity Tutorial

---
+++ Pong/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	static int player1Score = 0;
+	static int player2Score = 0;
+
+	public GUISkin scoreSkin;
+
+	public static void Score (string wallName) {
+		if(wallName == "rajtWål") {
+			player2Score +=1;
+		} else if(wallName == "LEFTWALL") {
+			player1Score +=1;
+		}
+	}
+
+
+	void OnGUI() {
+		GUI.skin = scoreSkin;
+		GUI.Label (new Rect (Screen.width / 2 - 150-12, 20, 100, 100), "" + player2Score);
+		GUI.Label (new Rect (Screen.width / 2 + 150-12, 20, 100, 100), "" + player1Score);
+	}
+}
---
+++ Pong/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public KeyCode moveUp;
+	public KeyCode moveDown;
+
+	public float speed;
+	// Update is called once per frame
+
+	private Rigidbody2D rb;
+
+	void Start() {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	void Update () {
+		if (Input.GetKey (moveUp)) {
+			rb.velocity = new Vector2(0,speed);
+		} else if (Input.GetKey (moveDown)) {
+			rb.velocity = new Vector2(0,-speed);
+		} else {
+			rb.velocity = new Vector2(0,0);
+		}
+	}
+}
